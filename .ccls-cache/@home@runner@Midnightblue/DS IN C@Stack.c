c25ea935b4146e520d68429dea70e63d45c15d65
virtuoso-04 <120053600+virtuoso-04@users.noreply.github.com>
2024-02-04 10:35:29 +0000

    recent changes

--- .ccls-cache/@home@runner@Midnightblue/DS IN C@Stack.c
+++ .ccls-cache/@home@runner@Midnightblue/DS IN C@Stack.c
@@ -1,6 +1,26 @@
 #include<stdio.h>
+#define MAX 5
 
-int main()
+int top=-1;
+int stack[MAX];
+void push(int);
+void pull();
+void peek();
+void dispaly();
+
+int main();
 {
+  int ch,item;
+  while(1)
+    {
+       printf("1. push an element in the stack \n");
+      printf("2. pop an element from the stackn \n");
+      printf("3. peek an ement from the stack \n");
+      printf("display the elements in the stack \n");
+       printf("enter choice \n");
+    scanf("%d",&ch);
+  
+  
+    
     
 }
